a23815f1c4899a25d6601d748a1f7f3a4c50691b
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-18 08:54:49 +0100

    Implement the append method of the Ring buffer challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # store the item in a buffer at using self.current as the index
+    self.storage[self.current] = item
+
+    # in deciding what self.current should be, we need to make sure haven't exceeded the capacity of our buffer,
+    # if we do, we reset reset current to 0, so we overwrite the first element and continue down
+    if self.current == self.capacity - 1:
+      self.current = 0
+    self.current += 1
 
   def get(self):
     pass
