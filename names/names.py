f01d82fdd8078cac34454da98c4479ecc72f0712
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-18 09:23:38 +0100

    implement solution one of optimizing the performance of the algorithm using Binary Search

--- names/names.py
+++ names/names.py
@@ -11,10 +11,20 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# SOLUTION (1): Runs in 0.15 seconds
+bst = BinarySearchTree(names_1[0])
+for name in names_2:
+    bst.insert(name)
+
+for name in names_1:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
